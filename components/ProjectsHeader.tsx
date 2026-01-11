9f757ff235fd3eb13727e7e20604ba949a3b5297
ajayvdas2 <ajayvdas2@gmail.com>
2026-01-11 17:38:31 +0530

    UI: completed ProjectsHeader

--- components/ProjectsHeader.tsx
+++ components/ProjectsHeader.tsx
@@ -1,5 +1,20 @@
+import { Link, Plus } from "lucide-react";
+import { SidebarTrigger } from "./ui/sidebar";
+
 export default function ProjectsHeader() {
     return (
-        <div>Projects Header</div>
+        <header className="flex items-center justify-between p-2 border-b">
+            <div className="flex items-center gap-2">
+                <SidebarTrigger className="h-8 w-8 rounded-lg hover:bg-accent text-muted-foreground" />
+                <p className="text-base font-medium text-foreground">Projects</p>
+            </div>
+            <div className="flex items-center gap-2">
+                <Link className="h-4 w-4" />
+                <span className="flex items-center text-base font-medium text-foreground">
+                    <Plus className="h-4 w-4" size={16} />
+                    <p className="ml-2">Add Project</p>
+                </span>
+            </div>
+        </header>
     )
 }
